1a39c231cfc919e0a7f4b85efb4cae4af2d8ae2f
Paul Connolly <paulconn5@gmail.com>
2022-09-18 21:58:52 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double creditBalance = 5000;
+        double interest1 = creditBalance*1.17;
+        double interest2 = creditBalance*1.17*1.17;
+
+        System.out.println("The credit balance after 1 month is $"
+        + interest1 + " and afer 2 months is $" + interest2);
     }
 }
